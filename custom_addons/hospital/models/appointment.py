c49cada2e771ac6c48fb81b6087b45d093e69fc0
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-08-10 18:37:13 +0300

    add two types of buttons & confirmation massage & help massage

--- custom_addons/hospital/models/appointment.py
+++ custom_addons/hospital/models/appointment.py
@@ -33,3 +33,6 @@ class HospitalAppointment(models.Model):
     @api.onchange('patient_id')
     def _onchange_patient_id(self):
         self.ref = self.patient_id.ref
+
+    def action_test(self):
+        print('test!!!!!!!!!!!!!!!!!!!!')
