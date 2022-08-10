02b18c8781e55509f9d66ed1259c6888f7f8efd2
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-08-10 14:19:51 +0300

    add _rec_name to appointment

--- custom_addons/hospital/models/appointment.py
+++ custom_addons/hospital/models/appointment.py
@@ -7,6 +7,7 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'ref'
 
     patient_id = fields.Many2one('hospital.patient', 'Patient')
     appointment_time = fields.Datetime(
