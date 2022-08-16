5c03e41521e85974236af08206ed351d7433f126
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-08-16 17:05:54 +0300

    adding transient model, cancel.appintment, and edited its footer

---
+++ custom_addons/hospital/wizard/cancel_appointment.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = 'cancel.appointment.wizard'
+    _description = 'Cancel Appointment Wizard'
+
+    appointment_id = fields.Many2one(
+        string='Appointment',
+        comodel_name='hospital.appointment'
+    )
+
+    def action_cancel(self):
+        return
