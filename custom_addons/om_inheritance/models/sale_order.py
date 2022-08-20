13a22540eb707b6b99d42f31a2f41e3425b2326a
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-08-20 15:31:54 +0300

    testing scaffold and inheritance

---
+++ custom_addons/om_inheritance/models/sale_order.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+
+# class om_inheritance(models.Model):
+#     _name = 'om_inheritance.om_inheritance'
+#     _description = 'om_inheritance.om_inheritance'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
