6f60c0b61780dab32719d047435238c35919e37c
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-08-21 22:24:50 +0300

    67->70 scaffold command and inheritance

--- custom_addons/om_inheritance/models/sale_order.py
+++ custom_addons/om_inheritance/models/sale_order.py
@@ -6,17 +6,12 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    confirmed_user_id = fields.Many2one(
+        string='Confirmed User',
+        comodel_name='res.users',
+    )
 
-# class om_inheritance(models.Model):
-#     _name = 'om_inheritance.om_inheritance'
-#     _description = 'om_inheritance.om_inheritance'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        print('Hi, I am working!! ..................')
+        self.confirmed_user_id = self.env.user.id
